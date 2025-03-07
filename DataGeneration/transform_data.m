81cd32561debde2d18ea9c3fc509b53b5921fd79
Diogo Miguel Batista de Sousa Correia da Costa <dmbscosta@gmail.com>
2025-03-07 18:50:07 +0200

    kaiser window

--- DataGeneration/transform_data.m
+++ DataGeneration/transform_data.m
@@ -10,22 +10,32 @@ function resized_images = transform_data(signal, noisy_signal, N, image_size, re
     noisy_signal = [noisy_signal, zeros(size(noisy_signal, 1), N - length(noisy_signal))];
     signal = [signal, zeros(size(signal, 1), N - length(signal))];
     
-    % Compute SST and VSST for noisy signal
-    [~, SST_noisy, VSST_noisy, ~, ~, ~, ~] = sst2_new(noisy_signal, 1 / sigma^2 / N, N, 0);
-    SST_noisy = SST_noisy(1:N/2, 1:original_size);
-    VSST_noisy = VSST_noisy(1:N/2, 1:original_size);
-
-    % Compute SST and VSST for clean signal
-    [~, SST_clean, VSST_clean, ~, ~, ~, ~] = sst2_new(signal, 1 / sigma^2 / N, N, 0);
-    SST_clean = SST_clean(1:N/2, 1:original_size);
-    VSST_clean = VSST_clean(1:N/2, 1:original_size);
-    
-    if transform == "SST"
+    if transform == "kaiser"
+        [SST_clean,~,~] = fsst(signal, 100e6,1024,'yaxis');
+        [SST_noisy,~,~] = fsst(noisy_signal, 100e6,1024,'yaxis');
+        SST_clean = SST_clean(1:512, 1:original_size);
+        SST_noisy = SST_noisy(1:512, 1:original_size);
         resized_images.transform_resized = imresize(SST_clean, [image_size, image_size], resize_method, "Antialiasing", false);
         resized_images.transform_noisy_resized = imresize(SST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
+        return;    
     else
-        resized_images.transform_resized = imresize(VSST_clean, [image_size, image_size], resize_method, "Antialiasing", false);
-        resized_images.transform_noisy_resized = imresize(VSST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
+        % Compute SST and VSST for noisy signal
+        [~, SST_noisy, VSST_noisy, ~, ~, ~, ~] = sst2_new(noisy_signal, 1 / sigma^2 / N, N, 0);
+        SST_noisy = SST_noisy(1:N/2, 1:original_size);
+        VSST_noisy = VSST_noisy(1:N/2, 1:original_size);
+
+        % Compute SST and VSST for clean signal
+        [~, SST_clean, VSST_clean, ~, ~, ~, ~] = sst2_new(signal, 1 / sigma^2 / N, N, 0);
+        SST_clean = SST_clean(1:N/2, 1:original_size);
+        VSST_clean = VSST_clean(1:N/2, 1:original_size);
+        
+        if transform == "SST"
+            resized_images.transform_resized = imresize(SST_clean, [image_size, image_size], resize_method, "Antialiasing", false);
+            resized_images.transform_noisy_resized = imresize(SST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
+        else
+            resized_images.transform_resized = imresize(VSST_clean, [image_size, image_size], resize_method, "Antialiasing", false);
+            resized_images.transform_noisy_resized = imresize(VSST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
+        end
     end
 end
 
