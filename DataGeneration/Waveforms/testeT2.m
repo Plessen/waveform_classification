a713b90dc55b3243e50788f620b5cd3e58b5aa3e
Diogo Miguel Batista de Sousa Correia da Costa <dmbscosta@gmail.com>
2025-04-11 19:44:41 +0300

    Bug correction

--- DataGeneration/Waveforms/testeT2.m
+++ DataGeneration/Waveforms/testeT2.m
@@ -6,9 +6,9 @@ T_seg = T_pulse / Ng;    % Duration of each segment
 pw = T_pulse;
 phi = zeros(size(t));    % Initialize phase vector
 
-for jj = 1:Ng
+for jj = 0:Ng-1
     % Find indices corresponding to segment jj
-    idx = (t >= (jj-1)*T_seg) & (t < jj*T_seg);
+    idx = (t >= (jj)*T_seg) & (t < (jj + 1)*T_seg);
     t_seg = t(idx) - (jj-1)*T_seg;
     t_seg = t_seg - t_seg(1);
     phi_quant = mod(2*pi/Nps*floor((Ng*t_seg-jj*pw)*(2*jj-Ng+1)/pw*Nps/2),2*pi);
