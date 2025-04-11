2b2c4f38f0d67cc8586e292936195fa32bc97951
Diogo Miguel Batista de Sousa Correia da Costa <dmbscosta@gmail.com>
2025-04-11 20:02:21 +0300

    Bug correction again...

--- DataGeneration/Waveforms/testeT2.m
+++ DataGeneration/Waveforms/testeT2.m
@@ -9,7 +9,7 @@ phi = zeros(size(t));    % Initialize phase vector
 for jj = 0:Ng-1
     % Find indices corresponding to segment jj
     idx = (t >= (jj)*T_seg) & (t < (jj + 1)*T_seg);
-    t_seg = t(idx) - (jj-1)*T_seg;
+    t_seg = t(idx) - (jj)*T_seg;
     t_seg = t_seg - t_seg(1);
     phi_quant = mod(2*pi/Nps*floor((Ng*t_seg-jj*pw)*(2*jj-Ng+1)/pw*Nps/2),2*pi);
     phi(idx) = phi_quant;
