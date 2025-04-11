b034619c62173bc3b46a656f6e08de2c86b924e0
Diogo Miguel Batista de Sousa Correia da Costa <dmbscosta@gmail.com>
2025-04-12 00:49:00 +0300

    use matlab implementation

--- DataGeneration/transform_data_faster.m
+++ DataGeneration/transform_data_faster.m
@@ -7,14 +7,19 @@ function resized_images = transform_data_faster(signal, SNR, N, image_size, resi
     noisy_signal = [noisy_signal, zeros(size(noisy_signal, 1), N - length(noisy_signal))];
 
     if transform == "SST" || transform == "VSST" || transform == "STFT"
-        [STFT, SST_noisy, VSST_noisy, ~, ~, ~, ~] = sst2_new(noisy_signal, 1 / sigma^2 / N, N, 0);
-        SST_noisy = SST_noisy(1:N/2, 1:original_size);
-        VSST_noisy = VSST_noisy(1:N/2, 1:original_size);
+        n = 1024;
+        t = -0.5:1/n:0.5-1/n;t=t';
+        g =  1/sigma*exp(-pi/sigma^2*t.^2);
+        
+        SST_noisy = fsst(noisy_signal, fs, g, 'yaxis');
+        %[STFT, SST_noisy, VSST_noisy, ~, ~, ~, ~] = sst2_new(noisy_signal, 1 / sigma^2 / N, N, 0);
+        SST_noisy = SST_noisy(N/2:N, 1:original_size);
+        %VSST_noisy = VSST_noisy(1:N/2, 1:original_size);
 
         if transform == "SST"
             resized_images.transform_resized = imresize(SST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
         elseif transform == "VSST"
-            resized_images.transform_resized = imresize(VSST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
+            resized_images.transform_resized = imresize(SST_noisy, [image_size, image_size], resize_method, "Antialiasing", false);
         else
             resized_images.transform_resized = imresize(STFT, [image_size, image_size], resize_method, "Antialiasing", false);
         end
