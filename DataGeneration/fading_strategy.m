a6a36ea69189a9e551ec4be23e5ea9aa6a42637a
Diogo Miguel Batista de Sousa Correia da Costa <dmbscosta@gmail.com>
2025-04-25 22:32:41 +0300

    New strategy fading channel

--- DataGeneration/fading_strategy.m
+++ DataGeneration/fading_strategy.m
@@ -63,6 +63,23 @@ function noisy_signal = fading_strategy(signal, SNR, strategy, Nr, numPaths_rang
         end
 
         noisy_signal = sum(aligned_signals, 2);
-    else
+    elseif strategy  == "selection"
+        PathNum = randi(numPaths_range,1,1);
+        pathDelayConstant = randi(pathDelay_range,1,1);
+        avgPathGainsConstant = randi(pathGain_range,1,1);
+        
+        pathDelays = [0:PathNum-1].*pathDelayConstant*1e-9;
+        avgPathGains = -1*[0:PathNum-1].*avgPathGainsConstant;
+        K = randi(Kfactor_range,1,1);
+
+        mimoChan = comm.MIMOChannel("SampleRate",fs,"FadingDistribution","Rician", "PathDelays",pathDelays, "AveragePathGains",avgPathGains,...
+        "KFactor",K, "MaximumDopplerShift", 0.001, "ChannelFiltering",true, "ReceiveCorrelationMatrix",eye(Nr), "TransmitCorrelationMatrix",1, "SpatialCorrelationSpecification","Separate Tx Rx",...
+        "NormalizeChannelOutputs",false);
+
+        fadingOutput = mimoChan(signal.');
+        received_signal = awgn(fadingOutput, SNR, "measured");
+        
+        [~, index] = max(sum(abs(received_signal).^2, 1));
+        noisy_signal = received_signal(:, index);
     end
 end
